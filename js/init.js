cba998fd7f2b8dc0f8e68dac0ad5bace39f7b71d
Thijs Boom <twboom@hotmail.nl>
2021-04-07 19:21:40 +0200

    Added autoload

--- js/init.js
+++ js/init.js
@@ -4,11 +4,14 @@ function init() {
         createButtons();
         data.ready = true;
         document.removeEventListener('mousemove', init);
+        document.removeEventListener('click', init);
+        clearTimeout(initTimout)
         console.log(`Completed initiation`)
     }
     else {
         console.warn(`Initiation failed`)
     }
 }
-
+const initTimout = setTimeout(init, 500)
 document.addEventListener('mousemove', init)
+document.addEventListener('click', init)
