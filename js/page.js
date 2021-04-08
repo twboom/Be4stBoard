543335c9db10a43bf75be576509f7eb8761a1949
Thijs Boom <twboom@hotmail.nl>
2021-04-08 16:13:20 +0200

    Made thing for easy page making thingy, yes

--- js/page.js
+++ js/page.js
@@ -1,18 +1,73 @@
 page = {
-    'assets': ['header', 'footer']
+    'assets': ['header', 'footer'],
+    'links': [
+        {
+            'extension': 'css',
+            'directory': 'css/',
+            'location': 'head',
+            'tag': 'link',
+            'attributes': [
+                {
+                    'type': 'rel',
+                    'value': 'stylesheet'
+                }
+            ],
+            'files': ['common', 'responsive']
+        },
+        {
+            'extension': 'ico',
+            'directory': '',
+            'location': 'head',
+            'tag': 'link',
+            'attributes': [
+                {
+                    'type': 'rel',
+                    'value': 'shortcut icon'
+                },
+                {
+                    'type': 'type',
+                    'value': 'image/x-icon'
+                }
+            ],
+            'files': ['favicon']
+        }
+    ]
 }
 
 function build() {
-    const assets = page.assets
-    for (let i = 0; i < assets.length; i++) {
-        fetch(`assets/html/${assets[i]}.html`)
+    // Adding pieces of HTML to the page
+    for (let i = 0; i < page.assets.length; i++) {
+        const asset = page.assets[i]
+        fetch(`assets/html/${asset}.html`)
             .then(response => response.text())
             .then(html => {
-                document.querySelectorAll(assets[i]).forEach(el => {
+                document.querySelectorAll(asset).forEach(el => {
                     el.innerHTML = html
                 });
+            });
+    };
+
+    // Adding correct links to the page
+    for (let i = 0; i < page.links.length; i++) {
+        const category = page.links[i]
+        for (let x = 0; x < category.files.length; x++) {
+            const element = document.createElement(category.tag);
+            const name = `${category.directory}${category.files[x]}.${category.extension}`
+
+            // Setting correct attributes
+            for (let y = 0; y < category.attributes.length; y++) {
+                element.setAttribute(category.attributes[y].type, category.attributes[i].value)
+            };
+
+            
+            element.setAttribute('href', name)
+
+            console.log(element)
+            document.querySelectorAll(category.location).forEach(item => {
+                item.appendChild(element)
             })
-    }
-}
+        };
+    };
+};
 
 build()
