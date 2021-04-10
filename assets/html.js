34be5eec12e3b16a56b059d0030119eb7c76f56d
Thijs Boom <twboom@hotmail.nl>
2021-04-10 14:56:24 +0200

    Made basics working again

---
+++ assets/html.js
@@ -0,0 +1,34 @@
+/* HTML Templates */
+page.elements = []
+
+page.elements.push({ // Header
+    "parent": "header",
+    "content": `
+    <h1 class="header"><a class="header" id="menu-open">Be4stBoard</a></h1>
+    <p class="header">The original Be4stSie Soundboard!</p>
+    `
+})
+
+page.elements.push({ // Footer
+    "parent": "footer",
+    "content": `
+    <p class="footer">This app was made by <a href="https://thijsboom.com">Thijs Boom</a>!</p>
+    <p class="footer">Find more info <a href="info.html">here</a>!</p>
+    <p class="footer">Find the source code <a href="https://github.com/twboom/Be4stBoard">here</a>!</p>
+    `
+})
+
+page.elements.push({ // Menu
+    "parent": "nav",
+    "content": `
+    <div id="nav">
+        <h1 class="nav"><a href="index.html" class="nav">Menu</a></h1>
+            <ul class="nav">
+                <li><a href="index.html">Board</a> - The soundboard</li>
+                <li><a href="sources.html">Sources</a> - Find the sources of the sounds and some statistics!</li>
+                <li><a href="info.html">Info</a> - Info page</li>
+            </ul>
+        <button class="nav" id="menu-close">Close</button>
+    </div>
+    `
+})
--- js/page.js
+++ js/page.js
@@ -1,2 +1,12 @@
 /* Code for page building */
 const page = [];
+
+page.build = function() {
+    // Add elements to the page
+    for (let i = 0; i< page.elements.length; i++) {
+        const obj = page.elements[i];
+        document.querySelectorAll(obj.parent).forEach(el => {
+            el.innerHTML = obj.content
+        })
+    }
+}
