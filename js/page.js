72a1255d418cefdedb9517fa1f68c831f699d3fd
Thijs Boom <twboom@hotmail.nl>
2021-04-10 15:18:39 +0200

    Made menu working again

--- js/page.js
+++ js/page.js
@@ -1,6 +1,11 @@
 /* Code for page building */
 const page = [];
 
+page.config = { // Config
+    "suffix": " | Be4stBoard",
+    "menu": false
+}
+
 page.build = function() {
     // Add elements to the page
     for (let i = 0; i< page.elements.length; i++) {
@@ -8,5 +13,37 @@ page.build = function() {
         document.querySelectorAll(obj.parent).forEach(el => {
             el.innerHTML = obj.content
         })
+    }
+
+    // Add event listeners
+    document.getElementById('menu-open').addEventListener('click', page.menu);
+    document.getElementById('menu-close').addEventListener('click', page.menu);
+
+    // Add transitions
+    const transitions = document.createElement('link');
+    transitions.setAttribute('rel', 'stylesheet');
+    transitions.setAttribute('href', 'css/transition.css')
+    document.head.appendChild(transitions)
+
+    // Add title suffix
+    document.querySelectorAll('title').forEach(el => { el.innerHTML += page.config.suffix })
+
+    // Add favion
+    const link = document.createElement('link');
+    link.setAttribute('rel', 'shortcut icon');
+    link.setAttribute('href', 'favicon.ico')
+    document.head.appendChild(link)
+}
+
+page.menu = function() {
+    if (!page.config.menu) { // Show the menu
+        document.body.style.overflow = 'hidden';
+        document.getElementsByTagName('nav')[0].style.display = 'block';
+        page.config.menu = true;
+    }
+    else if (page.config.menu) { // Hide the menu
+        document.body.style.overflow = 'visible';
+        document.getElementsByTagName('nav')[0].style.display = 'none';
+        page.config.menu = false;
     }
 }
