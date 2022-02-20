3c4e5a262281d2c3f16344122d728530096ec769
Thijs Boom <twboom@hotmail.nl>
2022-02-21 00:15:58 +0100

    Cleanup and added controls

--- src/scripts/soundboard.js
+++ src/scripts/soundboard.js
@@ -1,37 +1,57 @@
 function soundboard() {
-    buttons = [...document.getElementsByClassName('sound-button')];
-
-    buttons.forEach(button => initButton(button));
+    let session = {
+        'volume': 0.5,
+    };
     
     function initButton(button) {
         const config = {
-            'src': button.dataset.src,
-            'volume': button.dataset.volume,
-        }
+            'src': button.dataset.src, // Path of the sound file
+            'gain': button.dataset.volume, // Gain of the sound to equalize the volume
+        };
 
-        console.log(initButton, config);
-
+        // Preload the sample
         const audio = new Audio(config.src);
         audio.load()
 
+        // Play the sample
         button.addEventListener('click', _ => {
-            playsound(config);
+            const gain = config.gain * session.volume;
+            const audio = new Audio(config.src);
+            const ctx = new AudioContext();
+            const source = ctx.createMediaElementSource(audio);
+            const gainNode = ctx.createGain();
+            gainNode.gain.value = gain;
+            source.connect(gainNode);
+            gainNode.connect(ctx.destination)
+            audio.play();
         });
 
+        // Add to favorites
+        button.addEventListener('contextmenu', _ => {
+
+        });
         
     }
 
-    function playsound({src, volume}) {
-        volume = 1;
-        const audio = new Audio(src);
-        const ctx = new AudioContext();
-        const source = ctx.createMediaElementSource(audio);
-        const gainNode = ctx.createGain();
-        gainNode.gain.value = volume;
-        source.connect(gainNode);
-        gainNode.connect(ctx.destination)
-        audio.play();
+    function setVolume() {
+        const slider = document.getElementById('volume-slider');
+        const display = document.getElementById('volume-display');
+        session.volume = parseInt(slider.value) / 100;
+        display.innerHTML = parseInt(slider.value) / 10;
+        if (slider.value <= 100 && slider.getAttribute('max', 110)) { slider.setAttribute('max', 100) }
     }
+
+    function init() {
+        // Volume slider
+        document.getElementById('volume-slider').addEventListener('input', setVolume);
+        setVolume();
+
+        // Sound buttons
+        buttons = [...document.getElementsByClassName('sound-button')];
+        buttons.forEach(button => initButton(button));
+    }
+
+    init();
 };
 
 soundboard();
