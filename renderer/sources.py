78cd3e0ae8c57231672834bbfba5be5d486eb1a5
Thijs Boom <twboom@hotmail.nl>
2022-02-21 14:29:59 +0100

    Made the source renderer working

--- renderer/sources.py
+++ renderer/sources.py
@@ -15,6 +15,51 @@ def sources(html):
     html = html.replace('{{source-count}}', str(source_count))
     html = html.replace('{{clipper-count}}', str(clipper_count))
 
+    source_list = []
+
+    for source in sources:
+        clipper = source['clipper']
+        name = source['name']
+        link = source['link']
+        platform = source['platform'].capitalize()
+        source_sounds = source['sounds']
+
+        sound_list = []
+
+        for sound in source_sounds:
+            sound = next(x for x in sounds if x["slug"] == sound )
+            name = sound['name']
+            slug = sound['slug']
+
+            if 'extension' in sound:
+                extension = sound['extension']
+            else:
+                extension = 'mp3'
+
+            path = f'sounds/{slug}.{extension}'
+            src_attr = f'data-src="{path}"'
+
+            sound_html = f'<li class="sound-preview" {src_attr}>{name}</li>'
+
+            sound_list.append(sound_html)
+
+        sound_html = ''.join(sound_list)
+
+        source_html = f'''
+        <section>
+            <h1>{name}</h1>
+            <p>Clipped by: {clipper}</p>
+            <p>Find it <a href="{link}">here</a> on {platform}!</p>
+            <ul>
+                {sound_html}
+            </ul>
+        </section>
+        '''
+
+        source_list.append(source_html)
     
+    source_list = ''.join(source_list)
+
+    html = html.replace('{{sources}}', source_list)
 
     return html
